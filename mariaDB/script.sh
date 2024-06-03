1cf26236b1a307896297d816066f3eb551657dc7
mehdi sekouni <esekouni@esekouni.42.fr>
2024-06-03 05:04:50 -0400

    mariadb working now

--- mariaDB/script.sh
+++ mariaDB/script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-USER="mehdi"
-PASSWORD="1234"
-DATABASE_NAME="database_$USER"
+echo "FLUSH PRIVILEGES;" > data.sql
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;" >> data.sql
+echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" >> data.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER'@'%';" >> data.sql
+echo "FLUSH PRIVILEGES;" >> data.sql
 
-echo "FLUSH PRIVILEGES;" | mysql -u root -e --bootstrap
+mariadbd -u mysql --bootstrap < data.sql
 
-echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" | mysql -u root -e --bootstrap
+$@
 
-echo "CREATE USER IF NOT EXISTS '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';" | mysql -u root -e --bootstrap
-
-echo "GRANT ALL PRIVILEGES ON $DATABASE TO '$USER'@'localhost';" | mysql -u root -e --bootstrap
-
-echo "FLUSH PRIVILEGES;" | mysql -u root -e --bootstrap
-
-exec $@
+# exec "$@"
